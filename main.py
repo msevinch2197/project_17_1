ba03538edc0fac36cbeb5cb55988a7707e989abf
msevinch2197 <msevinch2197@gmail.com>
2026-02-17 16:28:29 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+n = int(input())
+numbers = []
+
+for i in range(n):
+    numbers.append(int(input()))
+
+for i in range(len(numbers)):
+    for j in range(0, len(numbers) - i - 1):
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+print(numbers)
